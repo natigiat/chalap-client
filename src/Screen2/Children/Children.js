90b2bc89f4656dc07b98ea3b1f05068f55a49018
shadmitsy <shadmitsy@gmail.com>
2020-07-20 16:15:14 +0300

    create children component

--- src/Screen2/Children/Children.js
+++ src/Screen2/Children/Children.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Children() {
-  return <div className="App">Children</div>;
+function Children(props) {
+  return (
+    <div className="App">
+      <span>{props.name}</span>
+      <span>{props.phone}</span>
+      <span>{props.school}</span>
+    </div>
+  );
 }
 
 export default Children;
