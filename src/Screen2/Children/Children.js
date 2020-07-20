03e3eb1bc52ef16434789faf053d5fa4308af731
shadmitsy <shadmitsy@gmail.com>
2020-07-20 16:29:00 +0300

    fix conflict

--- src/Screen2/Children/Children.js
+++ src/Screen2/Children/Children.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 function Children(props) {
-<<<<<<< HEAD
   return (
     <div className="App">
       <span>{props.name}</span>
@@ -9,9 +8,6 @@ function Children(props) {
       <span>{props.school}</span>
     </div>
   );
-=======
-  return <div></div>;
->>>>>>> 5f1072f36f8ebd15ba553c111961db916f77165a
 }
 
 export default Children;
