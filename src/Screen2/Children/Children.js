ef088ef207c2293fa02729a22726b27becd844e7
yosefcode <67313446+yosefcode@users.noreply.github.com>
2020-07-20 18:30:06 +0300

    chnga

--- src/Screen2/Children/Children.js
+++ src/Screen2/Children/Children.js
@@ -9,7 +9,7 @@ function Children(props) {
         <div className="name">{props.name}</div>
         <div>
           <span className="exit"> {props.exit}</span>
-          <span>----></span>
+          <span> ----> </span>
           <span className="school">{props.school}</span>
         </div>
       </div>
