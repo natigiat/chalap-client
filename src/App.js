cecff14f4ca2d9f0f87e75e0c9233e15d72c11f3
OrielBenMoshe <ori.l.2m@gmail.com>
2020-07-20 18:27:27 +0300

    Screen4 - Add ShuttleTravelRoute component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Flowchart_Stations from "./Screen4/Flowchart_Stations";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import ShuttleTravelRoute from "./Screen4/ShuttleTravelRoute";
 
 function App() {
   return (
     <div className="App">
-      <Flowchart_Stations />
+      <ShuttleTravelRoute />
     </div>
   );
 }
