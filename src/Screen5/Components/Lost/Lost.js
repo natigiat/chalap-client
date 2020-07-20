746e50b74031737114712ff0c051d150efd3f2be
barakformanski <barakformanski@gmail.com>
2020-07-20 18:40:32 +0300

    create component losts

---
+++ src/Screen5/Components/Lost/Lost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+import './Lost.css'
+function Lost() {
+    return (
+        <forms>
+            <h1>אבידה</h1>
+
+            <DatePicker />
+        </forms>
+    )
+}
+export default Lost;
