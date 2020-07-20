f41b4d405dea376590f821739f32a17647b68d43
David Reinuss <davideinat@gmail.com>
2020-07-20 14:33:34 +0300

    map.David

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Bootcamp</div>;
+  return <div className="App">Bootcamp;</div>;
 }
 
 export default App;
