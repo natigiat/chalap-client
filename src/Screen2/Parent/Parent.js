74527dbf0f15fc0d61368f618bac822234cb5b04
boaz katz <boazkatz89@gmail.com>
2020-07-20 15:41:30 +0300

    screen2 -api im parent

--- src/Screen2/Parent/Parent.js
+++ src/Screen2/Parent/Parent.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Alert from "../Alert/Alert";
 import Children from "..//Children/Children";
+
 function Parent() {
+  const [Api, setapi] = useState([
+    {
+      name: boaz,
+      phone: 052374245,
+      school: moria,
+    },
+    {
+      name: elior,
+      phone: 0523,
+      school: horev,
+    },
+    {
+      name: yosef,
+      phone: 052375,
+      school: moria,
+    },
+    {
+      name: shadmit,
+      phone: 054245,
+      school: shilo,
+    },
+  ]);
+
   return (
     <div>
       <div>
         <Alert />
-        <Children />
+
+        {Api.map((child) => (
+          <Children
+            name={child.name}
+            phone={child.phone}
+            school={child.school}
+          />
+        ))}
       </div>
     </div>
   );
