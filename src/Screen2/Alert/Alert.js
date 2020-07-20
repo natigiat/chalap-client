3b2fa4178480288523e4cbef89511aeb85f9f1ad
yosefcode <67313446+yosefcode@users.noreply.github.com>
2020-07-20 18:27:48 +0300

    change add

--- src/Screen2/Alert/Alert.js
+++ src/Screen2/Alert/Alert.js
@@ -3,11 +3,8 @@ import { Alert } from "antd";
 import "./alrts.css";
 function Alerts(props) {
   return (
-    <div>
-      screen alert
-      <div className="alrt1">
-        <Alert message={props.message} description={props.description} />
-      </div>
+    <div className="alrt1">
+      <Alert message={props.message} description={props.description} />
     </div>
   );
 }
