3405f2dbb7e857c5e4a411b60f33092ae1820aca
boaz katz <boazkatz89@gmail.com>
2020-07-20 20:07:10 +0300

    screen child onclick

--- src/Screen2/Children/Children.js
+++ src/Screen2/Children/Children.js
@@ -4,14 +4,12 @@ import "./Children.css";
 
 function Children(props) {
   return (
-    <div className="children">
-      <div className="child">
-        <div className="name">{props.name}</div>
-        <div>
-          <span className="exit"> {props.exit}</span>
-          <span> ----> </span>
-          <span className="school">{props.school}</span>
-        </div>
+    <div className="child" onClick={props.catchild}>
+      <div className="name">{props.name}</div>
+      <div>
+        <span className="exit"> {props.exit}</span>
+        <span> ----> </span>
+        <span className="school">{props.school}</span>
       </div>
     </div>
   );
