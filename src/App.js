07c068eba870e28a735ed8c4fa6bb82ab9b4fd33
arkady likovizki <arkados2@gmail.com>
2020-07-20 15:28:57 +0300

    password init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Password from "./Screen1/Password/Password";
+
 
 function App() {
   {
     window.document.title = "Bootcamp";
   }
-  return <div className="App">Bootcamp</div>;
+  return(
+
+    <div className="App">
+
+    <Password/>
+    </div> 
+  ) 
 }
 
 export default App;
