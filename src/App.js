962f0df0950aa72f022ec57ba76c29d173f45ed4
chayarosenberg <mushky43@gmail.com>
2020-07-20 19:44:54 +0300

    create schedule component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Schedule from "./Schedule/components/Schedule"
 
 function App() {
-  return <div className="App">Bootcamp</div>;
+  return (
+    <div>
+      <Schedule />
+    </div>
+  );
 }
 
 export default App;
