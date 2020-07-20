9328514a6ebdee6c8408b0c655c8c7476fc00180
OrNagar87 <or.hatzor@gmail.com>
2020-07-20 17:48:41 +0300

    start with component

--- src/Screen5/Components/Screen.js
+++ src/Screen5/Components/Screen.js
@@ -1,1 +1,12 @@
 import React from "react";
+import Button from "./"
+
+function Screen() {
+  const type = ["Losts", "Unusual Events", "Drive problems"];
+  const names = ["Ayelet Ben Dahan", "Shimrit Levi", "Yuval Dayan"];
+
+  
+  return(
+
+  )
+}
---
+++ src/Screen5/Components/TextArea.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Input } from "antd";
+
+export default () => {
+  const { TextArea } = Input;
+  return <TextArea rows={4} />;
+};
