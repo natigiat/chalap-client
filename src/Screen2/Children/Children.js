81246068ea288ed832d8b36fcd3a20369d78ef1d
shadmitsy <shadmitsy@gmail.com>
2020-07-20 17:19:31 +0300

    children update1

--- src/Screen2/Children/Children.js
+++ src/Screen2/Children/Children.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import { Button } from "antd";
+import "./Children.css";
 
 function Children(props) {
   return (
-    <div className="App">
-      <span>{props.name}</span>
-      <span>{props.phone}</span>
-      <span>{props.school}</span>
+    <div className="children">
+      <div>
+        <div>{props.name}</div>
+        <div>{props.phone}</div>
+        <div>{props.school}</div>
+      </div>
+
+      <div>
+        <Button type="primary">הרשאות</Button>
+        <Button type="primary">תחנה קרובה</Button>
+        <Button type="primary">מערכת שעות</Button>
+      </div>
     </div>
   );
 }
