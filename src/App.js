85da3de605d6013713df91d1f6afbfcd340edd34
natigiat <natigiat@gmail.com>
2020-07-21 17:00:41 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
 import Parent from "./Screen2";
+import { Router, Switch, Route } from "react-router";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
 function App() {
   return (
-    <div>
-      <Parent />
-    </div>
+    <Router history={history}>
+      <div className="main-wrapper">
+        <Switch>
+          <Route path={`/`} component={Parent} exact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
