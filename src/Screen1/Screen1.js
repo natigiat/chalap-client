83abb6fc4cbe721056b059f9e9100f6a4e753e39
hilelley <hilelley@gmail.com>
2020-07-21 12:47:27 +0300

    תרשל ןכומ

--- src/Screen1/Screen1.js
+++ src/Screen1/Screen1.js
@@ -3,30 +3,36 @@ import LogInValidation from "./component/LogInValidation/LogInValidation";
 import "./Screen1.css";
 import LogIn from "./component/LogIn/LogIn";
 import StudentSettingMenu from "./component/StudentSettingMenu/StudentSettingMenu";
+import StudentSettingUpdate from "./component/StudentSettingUpdate/StudentSettingUpdate";
 
 function Screen1() {
-  const [errorDemoToPassword, setErrorDemoToPassword] = useState(false);
-  const [errorDemoToNumPhon, setErrorDemoToNumPhon] = useState(false);
+  const [DemoToPassword, setDemoToPassword] = useState(false);
+  const [DemoToNumPhon, setDemoToNumPhon] = useState(false);
   window.document.title = "Bootcamp";
 
   const Demoserver = (value, from) => {
     if (value.length < 5 && from === "LogInValidation") {
-      setErrorDemoToPassword(true);
+      setDemoToPassword("incorrect");
     } else if (value.length >= 5 && from === "LogInValidation") {
-      setErrorDemoToPassword(false);
+      setDemoToPassword("correct");
     }
     if (value.length < 5 && from === "LogIn") {
-      setErrorDemoToNumPhon(true);
+      setDemoToNumPhon("incorrect");
     } else if (value.length >= 5 && from === "LogIn") {
-      setErrorDemoToNumPhon(false);
+      setDemoToNumPhon("correct");
     }
   };
   return (
-    <div className="screen1">
-      <LogIn check={Demoserver} message={errorDemoToNumPhon} />
+    <div className="page">
+      <div>
+        <LogIn check={Demoserver} message={DemoToNumPhon} />
+      </div>
       <br></br>
-      <LogInValidation check={Demoserver} message={errorDemoToPassword} />
+      <div>
+        <LogInValidation check={Demoserver} message={DemoToPassword} />
+      </div>
       {/* <StudentSettingMenu /> */}
+      {/* <StudentSettingUpdate /> */}
     </div>
   );
 }
--- src/Screen1/component/LogIn/LogIn.js
+++ src/Screen1/component/LogIn/LogIn.js
@@ -3,8 +3,8 @@ import { Input, Form, Checkbox, Button } from "antd";
 
 const LogIn = (props) => {
   const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+    labelCol: { span: 90 },
+    wrapperCol: { span: -10 },
   };
   const tailLayout = {
     wrapperCol: { offset: 8, span: 16 },
@@ -15,36 +15,38 @@ const LogIn = (props) => {
   const onFinish = (values) => {
     console.log("Success:", values);
     props.check(values.username, "LogIn");
-    setRander(1);
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
 
   return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <h4>הכנס מספר טלפון</h4>
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: "הכנס בבקשה מספר טלפון" }]}
+    <div className="logIn">
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          שלח סיסמה
-        </Button>
-      </Form.Item>
-      <Form.Item>
-        {!props.message && rander > 0 && <div>המספר אינו קיים במערכת</div>}
-      </Form.Item>
-    </Form>
+        <h4>הכנס מספר טלפון</h4>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "הכנס בבקשה מספר טלפון" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            שלח סיסמה
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          {props.message === "incorrect" && <div>המספר אינו קיים במערכת</div>}
+          {props.message === "correct" && <div>נשלחה סיסמה לטלפון שלך</div>}
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
