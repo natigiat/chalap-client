f5119780957bc1fbca803420b00c2be6ee07d1e3
OrNagar87 <or.hatzor@gmail.com>
2020-07-21 12:34:10 +0300

    update losts

--- src/Screen5/Components/Losts/Losts.js
+++ src/Screen5/Components/Losts/Losts.js
@@ -1,5 +1,17 @@
-import React from "react";
-import { Form, Input, Button, Select } from "antd";
+import React, { useState } from "react";
+import {
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+} from "antd";
+import "./Losts.css";
 
 function Losts() {
   const onFinish = (values) => {
@@ -8,15 +20,52 @@ function Losts() {
 
   const { TextArea } = Input;
 
+  // button chuse
+  const options = [
+    { label: "הלוך", value: "הלוך " },
+    { label: "חזור", value: "חזור" },
+  ];
+
+  const [choose, setChoose] = useState();
+
+  const onChange4 = (e) => {
+    setChoose(e.target.value);
+  };
+  console.log(choose);
   return (
     <Form name="control-hooks" onFinish={onFinish}>
-      <h1>אבידהד</h1>
-      <Form.Item
-        name="note"
-        label="תודה על מילה טובה"
-        rules={[{ required: true }]}
-      >
+      <h1>אבידות</h1>
+      <Form.Item name="שלום" label="פרטי האבידה" rules={[{ required: true }]}>
         <TextArea rows={10} />
+      </Form.Item>
+
+      <Form.Item label="למי אבד">
+        <Cascader
+          options={[
+            {
+              value: "zhejiang",
+              label: "Zhejiang",
+            },
+          ]}
+        />
+      </Form.Item>
+
+      <Form.Item label="מתי אבד">
+        <DatePicker />
+      </Form.Item>
+      <Form.Item label="באיזה הסעה">
+        <br />
+        <Radio.Group
+          options={options}
+          onChange={onChange4}
+          optionType="button"
+          buttonStyle="solid"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
       </Form.Item>
     </Form>
   );
--- src/Screen5/Components/Screen5.js
+++ src/Screen5/Components/Screen5.js
@@ -3,17 +3,17 @@ import GoodWord from "./GoodWord/GoodWord";
 import Complain from "./Complain/Complain";
 import Vioelence from "./Violence/Violence";
 import Losts from "./Losts/Losts";
+import "../Screen5.css";
 
 function Screen5() {
-  // const type = ["Losts", "Unusual Events", "Drive problems"];
-  // const names = ["Ayelet Ben Dahan", "Shimrit Levi", "Yuval Dayan"];
+  const names = ["Ayelet Ben Dahan", "Shimrit Levi", "Yuval Dayan"];
 
   return (
     <div>
-      <GoodWord />
-      <Complain />
-      <Vioelence />
-      <Losts />
+      <GoodWord children={[names]} type="good_word" />
+      <Complain children={[names]} type="report" />
+      <Vioelence children={[names]} type="vioelence" />
+      <Losts children={[names]} type="lost" />
     </div>
   );
 }
