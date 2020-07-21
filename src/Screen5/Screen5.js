5e85ab448ff9877c0a9143441e643f549588fdad
boaz katz <boazkatz89@gmail.com>
2020-07-21 14:13:28 +0300

    tast2

--- src/Screen5/Screen5.js
+++ src/Screen5/Screen5.js
@@ -1,12 +1,12 @@
 import React from "react";
-import GoodWord from "./components/GoodWord/GoodWord";
-import Complain from "./components/Complain/Complain";
-import Vioelence from "./components/Violence/Violence";
-import Losts from "./components/Losts/Losts";
+import GoodWord from "./Components/GoodWord/GoodWord";
+import Complain from "./Components/Complain/Complain";
+import Vioelence from "./Components/Violence/Violence";
+import Losts from "./Components/Losts/Losts";
 
 function Screen5() {
   // const type = ["Losts", "Unusual Events", "Drive problems"];
-  // const names = ["Ayelet Ben Dahan", "Shimrit Levi", "Yuval Dayan"];
+  // const names = ["Ayelet Ben Daha  n", "Shimrit Levi", "Yuval Dayan"];
 
   return (
     <div>
