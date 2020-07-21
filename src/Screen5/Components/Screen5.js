95452af9fbcc82e18e7c3f44c4251c8e31a912af
OrNagar87 <or.hatzor@gmail.com>
2020-07-21 13:51:56 +0300

    child-father relation

--- src/Screen5/Components/Screen5.js
+++ src/Screen5/Components/Screen5.js
@@ -7,13 +7,16 @@ import "../Screen5.css";
 
 function Screen5() {
   const names = ["Ayelet Ben Dahan", "Shimrit Levi", "Yuval Dayan"];
+  const onSubmit1 = (value) => {
+    console.log(value);
+  };
 
   return (
     <div>
-      <GoodWord children={[names]} type="good_word" />
-      <Complain children={[names]} type="report" />
-      <Vioelence children={[names]} type="vioelence" />
-      <Losts children={[names]} type="lost" />
+      <GoodWord children={[names]} type="good_word" onSubmit={onSubmit1} />
+      <Complain children={[names]} type="report" onSubmit={onSubmit1} />
+      <Vioelence children={[names]} type="vioelence" onSubmit={onSubmit1} />
+      <Losts children={[names]} type="lost" onSabmit={onSubmit1} />
     </div>
   );
 }
