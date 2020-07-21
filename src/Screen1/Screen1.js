16fe3938f3064811fe071a80a4c5395226f03e78
hilelley <hilelley@gmail.com>
2020-07-21 09:15:19 +0300

    colom clom clom

--- src/Screen1/Screen1.js
+++ src/Screen1/Screen1.js
@@ -10,30 +10,19 @@ function Screen1() {
   window.document.title = "Bootcamp";
 
   const Demoserver = (value, from) => {
-<<<<<<< HEAD
-    if (value.length < 5 && from === 'LogInValidation') {
-      setErrorDemoToPassword(true);
-    }
-    if (value.length < 5 && from === 'LogIn') {
-=======
     if (value.length < 5 && from === "LogInValidation") {
       setErrorDemoToPassword(true);
     }
     if (value.length < 5 && from === "LogIn") {
->>>>>>> e7793b2e1d78212a479623dd96e47a5d5e2bfa16
       setErrorDemoToNumPhon(true);
     }
   };
   return (
     <div className="screen1">
-      <LogIn check={Demoserver} message={errorDemoToNumPhon} />
-      <LogInValidation check={Demoserver} message={errorDemoToPassword} />
+      {/* <LogIn check={Demoserver} message={errorDemoToNumPhon} /> */}
       <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      {/* <StudentSettingMenu /> */}
+      {/* <LogInValidation check={Demoserver} message={errorDemoToPassword} /> */}
+      <StudentSettingMenu />
     </div>
   );
 }
