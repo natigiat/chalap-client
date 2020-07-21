2913392ac0fef90975757558a3332d6de0ca4da7
OrNagar87 <or.hatzor@gmail.com>
2020-07-21 09:40:51 +0300

    architecture

--- src/Screen5/Components/Complain/Complain.js
+++ src/Screen5/Components/Complain/Complain.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { Form, Input, Button, Select } from "antd";
 
-function Complain() {}
+function Complain() {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  const { TextArea } = Input;
+
+  return (
+    <Form name="control-hooks" onFinish={onFinish}>
+      <h1>מילה טובה</h1>
+      <Form.Item
+        name="note"
+        label="תודה על מילה טובה"
+        rules={[{ required: true }]}
+      >
+        <TextArea rows={10} />
+      </Form.Item>
+    </Form>
+  );
+}
+export default Complain;
--- src/Screen5/Components/Screen5.js
+++ src/Screen5/Components/Screen5.js
@@ -1,11 +1,21 @@
 import React from "react";
-import TextArea from "./TextArea";
+import GoodWord from "./GoodWord/GoodWord";
+import Complain from "./Complain/Complain";
+import Vioelence from "./Violence/Violence";
+import Losts from "./Losts/Losts";
 
-function Screen() {
-  const type = ["Losts", "Unusual Events", "Drive problems"];
-  const names = ["Ayelet Ben Dahan", "Shimrit Levi", "Yuval Dayan"];
+function Screen5() {
+  // const type = ["Losts", "Unusual Events", "Drive problems"];
+  // const names = ["Ayelet Ben Dahan", "Shimrit Levi", "Yuval Dayan"];
 
-  return <TextArea />;
+  return (
+    <div>
+      <GoodWord />
+      <Complain />
+      <Vioelence />
+      <Losts />
+    </div>
+  );
 }
 
-export default Screen;
+export default Screen5;
