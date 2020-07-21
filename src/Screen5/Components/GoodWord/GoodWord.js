30edc10b45014c6bf88cb9e5e2919f16dcb451fa
shadmitsy <shadmitsy@gmail.com>
2020-07-21 13:47:42 +0300

    change GoodWord

--- src/Screen5/Components/GoodWord/GoodWord.js
+++ src/Screen5/Components/GoodWord/GoodWord.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { Form, Input, Button, Select } from "antd";
+import React, { useState } from "react";
+import { Form, Input, Button, Select, DatePicker, Radio } from "antd";
+import "./GoodWord";
 
-function GoodWord() {
+function GoodWord(props) {
   const onFinish = (values) => {
     console.log(values);
+    props.onsubmit();
   };
 
+  const names = ["avi", "ruhama", "ttt"];
   const { TextArea } = Input;
+  const { Option } = Select;
+  const options = [
+    { label: "הלוך", value: "הלוך " },
+    { label: "חזור", value: "חזור" },
+  ];
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
 
+  const [choose, setChoose] = useState();
+  const onChange4 = (e) => {
+    setChoose(e.target.value);
+  };
+  const MakeItem = function (X) {
+    return <Option>{X}</Option>;
+  };
   return (
     <Form name="control-hooks" onFinish={onFinish}>
       <h1>מילה טובה</h1>
@@ -17,6 +35,38 @@ function GoodWord() {
         rules={[{ required: true }]}
       >
         <TextArea rows={10} />
+      </Form.Item>
+
+      <Form.Item>
+        <Select
+          defaultValue="בחר ילד"
+          style={{ width: 120 }}
+          onChange={handleChange}
+          placeholder="בחר ילד"
+        >
+          {names.map((value) => (
+            <Option>{value}</Option>
+          ))}
+        </Select>
+      </Form.Item>
+
+      <Form.Item>
+        <DatePicker />
+      </Form.Item>
+      <Form.Item label="באיזה הסעה">
+        <br />
+        <Radio.Group
+          options={options}
+          onChange={onChange4}
+          optionType="button"
+          buttonStyle="solid"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
       </Form.Item>
     </Form>
   );
--- src/Screen5/Screen5.js
+++ src/Screen5/Screen5.js
@@ -1,8 +1,8 @@
 import React from "react";
-import GoodWord from "./GoodWord/GoodWord";
-import Complain from "./Complain/Complain";
-import Vioelence from "./Violence/Violence";
-import Losts from "./Losts/Losts";
+import GoodWord from "./Components/GoodWord/GoodWord";
+import Complain from "./Components/Complain/Complain";
+import Vioelence from "./Components/Violence/Violence";
+import Losts from "./Components/Losts/Losts";
 
 function Screen5() {
   // const type = ["Losts", "Unusual Events", "Drive problems"];
